07c4c994086a8f8b265b4275166be4e8125a6804
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-22 16:14:06 -0300

    refatorado app para poder exibir a lista de tasks, criado o css do componente app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NewTask } from './components/NewTask'
 
 import logoToDo from './assets/logoToDo.svg'
 import './global.css'
+import styles from './App.module.css'
 import { Task } from './components/Task'
 
 interface ITasks {
@@ -19,23 +20,43 @@ const taskApp = [{
   id: 2,
   nameTask: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
   isCheck: false
+}, {
+  id: 3,
+  nameTask: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  isCheck: false
 }]
 
 function App() {
   return (
     <div className="App">
-      <header className='todoHeader'>
+      <header className={styles.todoHeader}>
         <img src={logoToDo} alt="logo da aplicação do do" />
       </header>
-      <div className='todoContent'>
+
+      <div className={styles.createNewTask}>
         <NewTask tasks={taskApp} />
+      </div>
+
+      <div className={styles.todoContent}>
+        <div className={styles.info}>
+          <div className={styles.tasksCreated}>
+            <p>Tarefas Criadas</p>
+            <span className={styles.count}>0</span>
+          </div>
+          <div className={styles.taskDone}>
+            <p>Concluidas</p>
+            <span className={styles.count}>0</span>
+          </div>
+
+        </div>
+
         {taskApp.map(tasks => {
           return (
             <Task key={tasks.id} id={tasks.id} name={tasks.nameTask} isCheck={tasks.isCheck} />
           )
         })}
       </div>
-    </div>
+    </div >
   )
 }
 
