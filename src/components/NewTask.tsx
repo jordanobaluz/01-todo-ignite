f4774dec6fc6f2152d113dc370577d01f5ad7b3f
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-24 21:51:24 -0300

    refatorado newTask para receber as novas tarefas e preencher no taskList

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,40 +1,22 @@
 import styles from './NewTask.module.css';
 import { PlusCircle } from 'phosphor-react';
 import { ChangeEvent, FormEvent, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
-interface iTaskApp {
-    id: number;
-    name: string;
-    isCheck: boolean
+
+interface iHandleNewTask {
+    handleNewTask: (taskContent: any) => void;
+    handleSubmit: (handleSubmit: any) => void;
+    newTask: string;
 }
 
-export function NewTask(taskApp: any) {
-    const [tasks, setTasks] = useState(Array<iTaskApp>)
-    const [newTask, setNewTask] = useState('');
-
-    function handleSubmit(event: FormEvent) {
-        event.preventDefault()
-        setTasks([...taskApp.tasks, {
-            id: uuidv4(),
-            name: newTask,
-            isCheck: false
-        }])
-        setNewTask('')
-    }
-
-    function handleNewTask(event: ChangeEvent<HTMLInputElement>) {
-        event.target.setCustomValidity('')
-        setNewTask(event.target.value)
-    }
-
+export function NewTask(taskApp: iHandleNewTask) {
     return (
-        <form onSubmit={handleSubmit} className={styles.newTask}>
+        <form onSubmit={taskApp.handleSubmit} className={styles.newTask}>
             <input
                 type="text"
                 className={styles.inputText}
-                value={newTask}
-                onChange={handleNewTask}
+                value={taskApp.newTask}
+                onChange={taskApp.handleNewTask}
                 placeholder='Adicione uma nova tarefa'
             />
             <button type='submit'>
