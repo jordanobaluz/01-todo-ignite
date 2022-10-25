9d9ab918cd8dc247e95fd4001a49d8f0d5e0acba
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-24 22:10:12 -0300

    refatorado a exibição das lista de tarefas e criado interface para receber via props as tarefas

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,36 +1,25 @@
 import styles from './Task.module.css';
-import { ClipboardText, Trash } from 'phosphor-react';
+import { Trash } from 'phosphor-react';
 
-interface ITasks2 {
+interface ITasks {
     task: {
         id: string;
         taskContent: string;
         status: boolean;
     }
-    taskListEmpty: boolean;
 }
 
-export function Task({ task, taskListEmpty }: ITasks2) {
+export function Task({ task }: ITasks) {
     return (
-        <>
-            <div className={styles.tasks}>
-                {task.taskContent ?
-                    <div className={styles.listTask}>
-                        <input type='checkbox' className={styles.checkbox} />
-                        <p>{task.taskContent}</p>
-                        <button>
-                            <Trash className={styles.trash} size={32} />
-                        </button>
-                    </div>
-                    :
-                    <div className={styles.empty}>
-                        <ClipboardText size={56} />
-                        <h1>VAZIO</h1>
-                    </div>
-
-                }
+        <div className={styles.tasks}>
+            <div className={styles.listTask}>
+                <input type='checkbox' className={styles.checkbox} />
+                <p>{task.taskContent}</p>
+                <button>
+                    <Trash className={styles.trash} size={32} />
+                </button>
             </div>
-        </>
+        </div>
     )
 }
 
